bd3f805bf69e1978229fd76b27b9d69c815ed41a
Rafael Bernardes Matias <143350593+matiasrbm@users.noreply.github.com>
2023-08-30 15:06:53 -0300

    Update Principal.java

--- Faculdade/Projeto01/Principal.java
+++ Faculdade/Projeto01/Principal.java
@@ -6,14 +6,14 @@ public class Principal {
 		
 		Pessoa p1 = new Pessoa();
 		p1.nome = "Francisco Molina";
-		p1.ra = "123456";
+		p1.matricula = "123456";
 		p1.idade = 40;
 		System.out.println(p1.nome);
 		p1.responderChamada(1);
 		
 		Pessoa p2 = new Pessoa();
 		p2.nome = "Rafael Matias";
-		p2.ra = "22303772";
+		p2.matricula = "22303772";
 		p2.idade = 18;
 		System.out.println(p2.nome);
 		p2.responderChamada(0);
