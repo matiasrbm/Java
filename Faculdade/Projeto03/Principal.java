a471dbd5d67ecbb6395c973af113ee0391175f4b
Rafael Bernardes Matias <143350593+matiasrbm@users.noreply.github.com>
2023-08-30 15:16:39 -0300

    Create Principal.java

---
+++ Faculdade/Projeto03/Principal.java
@@ -0,0 +1,51 @@
+package exemploLista;
+
+import java.util.ArrayList;
+
+public class Principal {
+	
+	public static void main(String[] args) {
+		
+		Figura f1 = new Figura();
+		f1.nome = "Quadrado";
+		f1.lados = 4;
+		
+		Figura f2 = new Figura();
+		f2.nome = "Pentagono";
+		f2.lados = 5;
+		
+		
+		Figura f3 = new Figura();
+		f3.nome = "Triângulo";
+		f3.lados = 3;
+		
+		Figura f4 = new Figura();
+		f4.nome = "Hexágono";
+		f4.lados = 6;
+		
+		Figura f5 = new Figura();
+		f5.nome = "Heptágono";
+		f5.lados = 3;
+		
+		ArrayList<Figura> listaFiguras = new ArrayList<Figura>();
+		
+		System.out.println("O tamanho da lista é:"
+				+ listaFiguras.size());
+		
+		listaFiguras.add(f1);
+		listaFiguras.add(f2);
+		listaFiguras.add(f3);
+		listaFiguras.add(f4);
+		listaFiguras.add(f5);
+		
+		System.out.println("O tamanho da lista é:"
+						+listaFiguras.size());
+		
+		for (int i = 0; i < listaFiguras.size(); i++) {
+			System.out.println(listaFiguras.get(i).nome);
+			System.out.println(listaFiguras.get(i).lados);
+			
+		}
+	}
+
+}
